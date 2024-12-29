820768ee8cc8db9def0a9d74d3f6eedda513534b
OldLiu <632171029@qq.com>
2024-12-29 19:20:40 +0800

    move out digitallab

---
+++ digitallab/2/build/obj_dir/main.d
@@ -0,0 +1,10 @@
+main.o: \
+ /home/oldliu/ysyx/ysyx-workbench/npc-verilog/digitallab/2/csrc/main.cpp \
+ /home/oldliu/ysyx/ysyx-workbench/nvboard/usr/include/nvboard.h \
+ /home/oldliu/ysyx/ysyx-workbench/nvboard/usr/include/pins.h Vtop.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h \
+ /home/oldliu/ysyx/ysyx-workbench/npc-verilog/digitallab/2/csrc/myautobind.h
